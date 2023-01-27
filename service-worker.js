29b0712440107d72f09d17f78106bd3d22b31c49
metacode22 <alohajune22@gmail.com>
2023-01-28 02:23:23 +0900

    fix: service-worker.js 수정

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = 'cache-v1';
-
-// 캐싱할 파일
-const FILES_TO_CACHE = ['/favicon.png'];
-
-// 상술한 파일 캐싱
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', () => {
+  // console.log('[Service Worker] installed');
 });
 
-// CACHE_NAME이 변경되면 오래된 캐시 삭제
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (CACHE_NAME !== key) return caches.delete(key);
-        })
-      )
-    )
-  );
+self.addEventListener('activate', () => {
+  // console.log('[Service Worker] actived', event);
+});
+
+self.addEventListener('fetch', () => {
+  // console.log('[Service Worker] fetched resource ' + event.request.url);
 });
